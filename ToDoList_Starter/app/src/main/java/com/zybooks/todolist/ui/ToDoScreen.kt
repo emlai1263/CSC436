68209d0ff6180195abab6e55784fef5cb46749a2
emlai1263 <nicecherrim@gmail.com>
2025-01-29 14:46:27 -0800

    remember dismiss state

--- ToDoList_Starter/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
+++ ToDoList_Starter/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DismissState
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -70,8 +72,23 @@ fun TaskList(
          items = taskList,
          key = { task -> task.id }
       ) { task ->
+         val currentTask by rememberUpdatedState(task)
          val dismissState = rememberDismissState(
-            // TODO: Add actions for item dismissal
+            confirmValueChange = {
+               when (it) {
+                  DismissValue.DismissedToEnd -> {
+                     onDeleteTask(currentTask)
+                     true
+                  }
+
+                  DismissValue.DismissedToStart -> {
+                     onArchiveTask(currentTask)
+                     true
+                  }
+
+                  else -> false
+               }
+            }
          )
 
          SwipeToDismiss(
