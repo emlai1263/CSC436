ad4433c5264b147f0798b013a488d4f03521ed58
emlai1263 <nicecherrim@gmail.com>
2025-02-10 21:36:33 -0800

    added preview and ran it

--- PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/MainActivity.kt
+++ PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.zybooks.petadoption.ui.PetApp
+import com.zybooks.petadoption.ui.PreviewListScreen
 import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,8 @@ class MainActivity : ComponentActivity() {
                modifier = Modifier.fillMaxSize(),
                color = MaterialTheme.colorScheme.background
             ) {
-               PetApp()
+//               PetApp()
+               PreviewListScreen()
             }
          }
       }
