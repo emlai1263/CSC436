31da118018de0c382d002a2c83321074df355c98
emlai1263 <nicecherrim@gmail.com>
2025-02-19 22:48:55 -0800

    notifications

--- Lab4(Timer)/CountdownTimer_Starter/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ Lab4(Timer)/CountdownTimer_Starter/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.zybooks.countdowntimer
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
@@ -23,6 +29,12 @@ import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
 
 class MainActivity : ComponentActivity() {
 
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
+
    private val timerViewModel = TimerViewModel()
 
    override fun onStop() {
@@ -30,7 +42,14 @@ class MainActivity : ComponentActivity() {
 
       // Start TimerWorker if the timer is running
       if (timerViewModel.isRunning) {
-         startWorker(timerViewModel.remainingMillis)
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                  Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+               startWorker(timerViewModel.remainingMillis)
+            }
+         } else {
+            startWorker(timerViewModel.remainingMillis)
+         }
       }
    }
 
@@ -56,6 +75,14 @@ class MainActivity : ComponentActivity() {
             ) {
                TimerScreen(timerViewModel = timerViewModel)
             }
+         }
+      }
+
+      // Only need permission to post notifications on Tiramisu and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
          }
       }
    }
